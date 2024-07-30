588ad13dc82018a9390303af38a74372ae9d7aed
IsaacShelton <isaac.a.shelton@gmail.com>
2024-07-29 20:18:40 -0500

    Added nvim-treesitter-context for helpful context

--- init.lua
+++ init.lua
@@ -240,6 +240,33 @@ require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   { 'chrisgrieser/nvim-spider', lazy = true },
 
+  -- Context plugin I like
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    dependencies = { 'gbprod/nord.nvim' },
+    opts = {
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    },
+    config = function(_, opts)
+      require('treesitter-context').setup(opts)
+      local colors = require('nord.colors').palette
+
+      vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = colors.polar_night.brightest })
+      vim.api.nvim_set_hl(0, 'TreesitterContextLineNumber', { bg = colors.polar_night.brightest })
+    end,
+  },
+
   {
     'max397574/better-escape.nvim',
     event = 'InsertEnter',
