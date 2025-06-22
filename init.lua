7502f40215d2499d567b4b3837c1919550965d76
IsaacShelton <isaac.a.shelton@gmail.com>
2025-06-21 23:30:03 -0500

    Added folding

--- init.lua
+++ init.lua
@@ -1593,7 +1593,31 @@ require('lazy').setup({
     'vxpm/ferris.nvim',
     opts = {},
   },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = 'kevinhwang91/promise-async',
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { 'lsp', 'ident' }
+      end,
+    },
+    config = function(_, opts)
+      vim.o.foldcolumn = '0' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 0
+      vim.o.foldenable = true
+      require('ufo').setup(opts)
 
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+      vim.keymap.set('n', 'zK', function()
+        local win = require('ufo').peekFoldedLinesUnderCursor()
+        if not win then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = 'Peek fold' })
+    end,
+  },
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
